6cb2c4668ddaa161eaaa8450d82d9477247cdd48
Mohammed Adnan A <codewithadnan2020@gmail.com>
2022-09-02 20:48:23 +0530

    project1_Rectified

--- project1.py
+++ project1.py
@@ -2,15 +2,12 @@
 
 def check_houses():
     name = input("Enter Your Name:");
-    records = [["adnan", "safa"], ["fazil", "hira"]];
+    records = {"adnan":"safa", "fazil": "hira"};
 
-    for record in records:
-        if record[0] == name:
-            print("You are from "+record[1]+" house.");
-            break;
-        else:
-            print("You are not registered.");
-            check_houses();
-            break;
+    if name in records:
+        print("You are from",records[name],"house.");
+    else:
+        print("You are not registaered.");
+        check_houses();
 
 check_houses();
